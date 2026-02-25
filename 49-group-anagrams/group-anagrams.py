9a6002f5082756752d721cd4879308cf17fad405
Mukund Senthil Kumar <136768407+SKMukund@users.noreply.github.com>
2026-02-24 21:47:27 -0800

    Time: 49 ms (9.53%) | Memory: 22.2 MB (7.44%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        answer = [[]]
+        seen = {}
+        for i in range(len(strs)):
+            current = {}
+            for char in strs[i]:
+                if char in current:
+                    current[char] += 1
+                else:
+                    current[char] = 1
+            key = tuple(sorted(current.items()))
+            
+            if key not in seen:
+                seen[key] = [strs[i]]
+            else:
+                seen[key].append(strs[i])
+
+        return list(seen.values())
+
+        
