4b6465b51ff8290d2f467c3ec6413443d15aea99
Mukund Senthil Kumar <136768407+SKMukund@users.noreply.github.com>
2026-02-28 13:37:09 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (99.43%) - LeetSync

---
+++ LeetCode/496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        result = []
+        next_map = {}
+        for num in nums2:
+            while stack and num > stack[-1]:
+                prev = stack.pop()
+                next_map[prev] = num
+            stack.append(num)
+        
+        while stack:
+            next_map[stack.pop()] = -1
+        
+        for num in nums1:
+            result.append(next_map[num])
+
+        return result
+    
+
+
+        
