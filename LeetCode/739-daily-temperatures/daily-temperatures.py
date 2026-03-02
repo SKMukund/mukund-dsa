d7704fa172f0239e3d3047c66655ea44fabc8648
Mukund Senthil Kumar <136768407+SKMukund@users.noreply.github.com>
2026-03-01 17:26:27 -0800

    Time: 145 ms (94.28%) | Memory: 27 MB (40.25%) - LeetSync

--- LeetCode/739-daily-temperatures/daily-temperatures.py
+++ LeetCode/739-daily-temperatures/daily-temperatures.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
-        result = [0] * len(temperatures)
         stack = []
+        result = [0] * len(temperatures)
+
         for i in range(len(temperatures)):
             while stack and temperatures[i] > temperatures[stack[-1]]:
                 prev = stack.pop()
                 result[prev] = i - prev
             stack.append(i)
         return result
-            
+        
