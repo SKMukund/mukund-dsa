3954d2f48645fb11eaeac60dee28cdc024c79978
Mukund Senthil Kumar <136768407+SKMukund@users.noreply.github.com>
2026-03-10 16:36:37 -0700

    Time: 163 ms (98.88%) | Memory: 20.8 MB (85.95%) - LeetSync

--- LeetCode/84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ LeetCode/84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -4,36 +4,43 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
+        if len(heights) == 1:
+            return heights[0]
+
         stack = []
         max_size = 0
-        smallest = sys.maxsize
-        for i in range(len(heights)):  
-            size = 0
+        
+        for i in range(len(heights)):
+            height = heights[i]
             width = 0
-            while stack and heights[i] < heights[stack[-1]]:
+            while stack and height <= heights[stack[-1]]:
                 prev = stack.pop()
                 if not stack:
-                    width = i
+                    width = i   
                 else:
-                    width = i - stack[-1] - 1
-                height = heights[prev]
-                size = height * width
+                    width = i - stack[-1] - 1 
+                
+                size = heights[prev] * width
+
                 if size > max_size:
                     max_size = size
+
             stack.append(i)
 
         i = len(heights)
         while stack:
             prev = stack.pop()
             if not stack:
-                width = i
+                width = i   
             else:
-                width = i - stack[-1] - 1
-            height = heights[prev]
-            size = height * width
+                width = i - stack[-1] - 1                
+            size = heights[prev] * width
             if size > max_size:
                 max_size = size
-        
         return max_size
-
+                    
+                
+                
+            
+            
         
