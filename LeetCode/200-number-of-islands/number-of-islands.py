98a0d708c57cde1c995cd95e28024b17c78102c9
Mukund Senthil Kumar <136768407+SKMukund@users.noreply.github.com>
2026-04-13 11:58:13 -0700

    Time: 225 ms (62.65%) | Memory: 27.8 MB (73.03%) - LeetSync

--- LeetCode/200-number-of-islands/number-of-islands.py
+++ LeetCode/200-number-of-islands/number-of-islands.py
@@ -4,26 +4,29 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        row = len(grid)
-        col = len(grid[0])
+
+        rows = len(grid)
+        cols = len(grid[0])
         islands = 0
 
-        def bfs(r, c):
-            if r < 0 or c < 0 or r >= row or c >= col:
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols:
                 return
-            if grid[r][c] == '0':
+            if grid[r][c] == "0":
                 return
-            
-            grid[r][c] = '0'
 
-            bfs(r + 1, c)
-            bfs(r - 1, c)
-            bfs(r, c + 1)
-            bfs(r, c - 1)
+            grid[r][c] = "0"
+            
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
         
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == '1':
-                    bfs(r, c)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r,c)
                     islands += 1
+        
         return islands
+        
